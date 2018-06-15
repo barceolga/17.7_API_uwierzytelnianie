53cfb1409ded53bd0485dd4528c93665193f71a1
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-15 09:36:29 +0200

    Add config.js file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-module.exports= {
+module.exports = {
   'GOOGLE_CLIENT_ID:': '919649091646-66bafq3nt3v2tfcs74hddee1si4smduu.apps.googleusercontent.com',
   'GOOGLE_CLIENT_SECRET': 'kFG7k4IIYozS5aV8AoInh054',
   'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
-}
+};
