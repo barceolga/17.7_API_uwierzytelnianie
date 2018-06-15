3dfb0b9f186656483284f6bdfd2bd457ece1f33e
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-15 09:37:00 +0200

    Update content for server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var config = require('./config');
+var config = require('./config.js');
 var app = express();
 var googleProfile = {};
 
@@ -18,11 +18,41 @@ passport.use(new GoogleStrategy({
         clientSecret: config.GOOGLE_CLIENT_SECRET,
         callbackURL: config.CALLBACK_URL
     },
-    function(accesToken, refreshToken, profile, cb) {
+    function(accessToken, refreshToken, profile, cb) {
       googleProfile = {
         id: profile.id,
-        dipslayName: profile.displayName
+        displayName: profile.displayName
       };
       cb(null, profile);
     }
-))
+));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app routes
+
+app.get('/', function(req, res) {
+  res.render('index', {user: req.user});
+});
+
+app.get('/logged', function(req, res) {
+  res.render('logged', {user: googleProfile});
+});
+
+//Passport routes
+
+app.get('auth/google',
+passport.authenticate('google', {
+scope: ['profile', 'email']
+}));
+
+app.get('auth/google/callback',
+    passport.autenticate('google', {
+      succesRedirect: '/logged',
+      failureRedirect: '/'
+    }));
+
+app.listen(3000);
