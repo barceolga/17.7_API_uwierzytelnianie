9b52967c9c18ee23a19debcbbf512aacc641c3db
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-17 10:31:10 +0200

    Update content for server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var config = require('./config.js');
+var config = require('./config');
 var app = express();
 var googleProfile = {};
 
